3d184bd498110f5d51490a846bcaf9293f73a950
toastsandwich <shreyasxcore2003@gmail.com>
2024-05-29 14:57:46 +0530

    added function to convert struct to grpc struct

--- utils/utils.go
+++ utils/utils.go
@@ -3,9 +3,25 @@ package utils
 import (
 	"encoding/json"
 	"net/http"
+
+	"github.com/toastsandwich/totality-assignment-GRPC-version/models"
+	"github.com/toastsandwich/totality-assignment-GRPC-version/pb/github.com/toastsandwich/totality-assignment-GRPC-version/pb"
 )
 
 func CreateAndWriteJSON(w http.ResponseWriter, v any) error {
 	encoder := json.NewEncoder(w)
 	return encoder.Encode(v)
+}
+
+func ConvertUserStructToGRPCUserStruct(u *models.User) *pb.User {
+	return &pb.User{
+		Fname:   u.FName,
+		Lname:   u.LName,
+		City:    u.City,
+		Id:      int32(u.ID),
+		Phone:   int32(u.Phone),
+		Height:  u.Height,
+		Married: u.Married,
+	}
+
 }
